a9bf95752c3c16a4a5a0a7b88994bdd198fe602d
Joseph Bangoura <ptijjo@hotmail.com>
2024-09-10 21:59:48 +0200

    enfin ça marche

---
+++ App.tsx
@@ -0,0 +1,20 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Bonjour !C'est moi qui ai fait ça !</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
